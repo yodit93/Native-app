480ba09a58c69f4e2a35e3d1d102d40ca971646c
yodit93 <yoditabebe9391@gmail.com>
2024-08-10 13:38:20 +0300

    Clearing input value after adding todo

--- components/addTodo.js
+++ components/addTodo.js
@@ -3,17 +3,20 @@ import {StyleSheet, View, TextInput, Button} from 'react-native';
 
 const AddTodo = ({updateTodo}) => {
     const [text, setText] = useState('');
-    const changeHandler = (val) => {
-        setText(val);
+
+    const handleSubmit = () => {
+        updateTodo(text);
+        setText('');
     };
     return (
         <View>
             <TextInput
                 style={styles.input}
-                onChangeText={changeHandler}
+                value={text}
+                onChangeText={setText}
                 placeholder='new todo...'
             />
-            <Button title={"Add Todo"} color='coral' onPress={() => updateTodo(text)}/>
+            <Button title={"Add Todo"} color='coral' onPress={handleSubmit}/>
         </View>
     );
 };
